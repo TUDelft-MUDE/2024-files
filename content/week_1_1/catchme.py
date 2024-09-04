88d6dbd025980762e51c6d8cf0ff2e3fc50dde97
Robert Lanzafame <R.C.Lanzafame@tudelft.nl>
2024-09-04 09:47:03 +0200

    PA

--- content/week_1_1/catchme.py
+++ content/week_1_1/catchme.py
@@ -1,5 +1,4 @@
-data = {
-    'Bulbasaur': {
+data = {'name': 'Bulbasaur',
         'type': 'Grass/Poison',
         'species': 'Seed Pokemon',
         'height': 0.7,
@@ -12,51 +11,5 @@ data = {
             'speed': 45
         },
         'evolves_to': 'Ivysaur',
-        'move_set': ['Growl', 'Tackle', 'Vine Whip'],
-    },
-    'Charmander': {
-        'type': 'Fire',
-        'species': 'Lizard Pokemon',
-        'height': 0.6,
-        'weight': 8.5,
-        'abilities': ['Blaze', 'Solar Power'],
-        'base_stats': {
-            'hp': 39,
-            'attack': 52,
-            'defense': 43,
-            'speed': 65
-        },
-        'evolves_to': 'Charmeleon',
-        'move_set': ['Growl', 'Scratch', 'Ember'],
-    },
-    'Squirtle': {
-        'type': 'Water',
-        'species': 'Tiny Turtle Pokemon',
-        'height': 0.5,
-        'weight': 9.0,
-        'abilities': ['Torrent', 'Rain Dish'],
-        'base_stats': {
-            'hp': 44,
-            'attack': 48,
-            'defense': 65,
-            'speed': 43
-        },
-        'evolves_to': 'Wartortle',
-        'move_set': ['Tackle', 'Tail Whip', 'Water Gun'],
-    },
-    'Pikachu': {
-        'type': 'Electric',
-        'species': 'Mouse Pokemon',
-        'height': 0.4,
-        'weight': 6.0,
-        'abilities': ['Static', 'Lightning Rod'],
-        'base_stats': {
-            'hp': 35,
-            'attack': 55,
-            'defense': 40,
-            'speed': 90
-        },
-        'evolves_to': 'Raichu',
-        'move_set': ['Growl', 'Quick Attack', 'Thunder Shock'],
-    }
-}
+        'move_set': ['Growl', 'Tackle', 'Vine Whip']
+}   
