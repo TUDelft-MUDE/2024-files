7f888d602c5b84998e7c162dea79400a2dfd0994
Robert Lanzafame <R.C.Lanzafame@tudelft.nl>
2024-09-22 15:52:51 +0200

    more GA 1.4

--- content/GA_1_4/functions.py
+++ content/GA_1_4/functions.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import ipywidgets as widgets
@@ -33,7 +34,7 @@ def plot_model(d, alt_model=None):
                     (y_hat + CI_Y_hat),
                     facecolor='orange',
                     alpha=0.4,
-                    label = f'{(1-d['alpha'])*100:.1f}%' + ' Confidence Region')
+                    label = f'{(1-d["alpha"])*100:.1f}%' + ' Confidence Region')
     
     if alt_model is not None:
         ax.plot(alt_model[1],
@@ -66,7 +67,7 @@ def plot_residual(d):
     ax.plot(times, e_hat, 'o', markeredgecolor='black', label='Residual')
     ax.plot(times, -CI_res,
              '--', color='orange', 
-             label = f'{(1-d['alpha'])*100:.1f}%' + ' Confidence Region')
+             label = f'{(1-d["alpha"])*100:.1f}%' + ' Confidence Region')
     ax.plot(times, +CI_res,
              '--', color='orange')
     
@@ -147,3 +148,68 @@ def xhat_slider_plot(A, y, t, Sigma_y=None):
 # Example usage
 # A, y, t should be defined before calling this function
 # xhat_slider_plot(A, y, t)
+
+def to_days_years(times):
+    '''Convert the observation times to days and years.'''
+    
+    times_datetime = pd.to_datetime(times)
+    time_diff = (times_datetime - times_datetime[0])
+    days_diff = (time_diff / np.timedelta64(1,'D')).astype(int)
+    
+    days = days_diff.to_numpy()
+    years = days/365
+    
+    return days, years
+
+def BLUE(d):
+    """Calculate the Best Linear Unbiased Estimator
+    
+    Uses dict as input/output:
+      - inputs defined from existing values in dict
+      - outputs defined as new values in dict
+    """
+
+    d['Sigma_X_hat'] = (np.linalg.inv(d['A'].T
+                        @ np.linalg.inv(d['Sigma_Y'])
+                        @ d['A']))
+    d['x_hat'] = (d['Sigma_X_hat']
+                  @ d['A'].T
+                  @ np.linalg.inv(d['Sigma_Y'])
+                  @ d['y'])
+    d['y_hat'] = d['A'] @ d['x_hat']
+    d['e_hat'] = d['y'] - d['y_hat']
+    d['Sigma_Y_hat'] = d['A'] @ d['Sigma_X_hat'] @ d['A'].T
+    d['std_y'] = np.sqrt(d['Sigma_Y_hat'].diagonal())
+    d['Sigma_e_hat'] = d['Sigma_Y'] - d['Sigma_Y_hat']
+    d['std_e_hat'] = np.sqrt(d['Sigma_e_hat'].diagonal())
+
+    return d
+
+def get_CI(d, alpha):
+    """Compute the confidence intervals.
+    
+    Uses dict as input/output:
+      - inputs defined from existing values in dict
+      - outputs defined as new values in dict
+    """
+
+    d['k'] = norm.ppf(1 - 0.5*alpha)
+    d['CI_y'] = d['k']*d['std_y']
+    d['CI_res'] = d['k']*d['std_e_hat']
+    d['CI_y_hat'] = d['k']*np.sqrt(d['Sigma_Y_hat'].diagonal())
+
+    return d
+
+def model_summary(d):
+    """Print the model summary."""
+
+    print('Summary of Model')
+    print('----------------')
+    print('  Data type:', d['data_type'])
+    print('  Model type:', d['model_type'])
+    print('  Number of observations:', len(d['times']))
+    print('  Model parameters:')
+    for i in range(len(d['x_hat'])):
+        print(f'    x_{i} = {d["x_hat"][i]:6.3f}'
+              + f'  +/- {np.sqrt(d["Sigma_X_hat"][i,i]):6.3f}')
+    print('----------------\n')
