cca0c97d1d4bca46e045f5282fee87066d5650be
GitLab CI <gitlab-ci@example.com>
2024-12-18 12:31:29 +0000

    Update synced files [skip ci]

--- synced_files/Week_2_6/WS_2_6_solution_clean.py
+++ synced_files/Week_2_6/WS_2_6_solution_clean.py
@@ -25,6 +25,7 @@ x_val = x_val.reshape(-1,1)
 
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.plot(data_x.flatten(), data_t, 'x', color='blue', markersize=10, label='Data')
+ax.plot(x_val, 0.8*x_val + 4.75, color='red', markersize=10, label='Ground truth')
 ax.set_title('Linear Data Example', fontsize=16)
 ax.set_xlabel('x', fontsize=14)
 ax.set_ylabel('t', fontsize=14)
@@ -54,6 +55,7 @@ for epoch in range(n_epochs):
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.plot(data_x, data_t, ".", markersize=20, label="Data")
 ax.plot(data_x, MLP_prediction, "-", markersize=10, label="Prediction")
+ax.plot(x_val, 0.8*x_val + 4.75, color='red', markersize=10, label='Ground truth')
 
 ax.set_title("Linear Data Example", fontsize=16)
 ax.set_xlabel("x", fontsize=14)
@@ -90,6 +92,7 @@ for epoch in range(n_epochs):
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.plot(data_x, data_t, ".", markersize=20, label="Data")
 ax.plot(x_val, MLP_valprediction, "-", markersize=10, label="Prediction")
+ax.plot(x_val, 0.8*x_val + 4.75, color='red', markersize=10, label='Ground truth')
 
 ax.set_title("Linear Data Example", fontsize=16)
 ax.set_xlabel("x", fontsize=14)
@@ -123,6 +126,7 @@ for epoch in range(n_epochs):
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.plot(data_x, data_t, ".", markersize=20, label="Data")
 ax.plot(x_val, MLP_valprediction, "-", markersize=10, label="Prediction")
+ax.plot(x_val, 0.8*x_val + 4.75, color='red', markersize=10, label='Ground truth')
 
 ax.set_title("Linear Data Example", fontsize=16)
 ax.set_xlabel("x", fontsize=14)
