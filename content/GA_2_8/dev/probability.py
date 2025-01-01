971697aaf52109a4afc70d24e8fb711ce53aef48
Robert Lanzafame <R.C.Lanzafame@tudelft.nl>
2025-01-01 14:57:37 +0100

    expected tickets good enough

--- content/GA_2_8/dev/probability.py
+++ content/GA_2_8/dev/probability.py
@@ -25,7 +25,8 @@ to be accessed in a variety of ways. For example:
 Each of these would be convenient to compute every 15 min, hour,
 day, etc, to make calculations faster and provide summary info.
 
-
+on init set_ticket_model sets up a dict and will create a function
+to modify, change dict value then run self.get_ticket_model()
 
 """
 import scipy.stats as stats
@@ -37,9 +38,17 @@ from datetime import datetime, timedelta
 import pickle
 
 class Probability:
-    def __init__(self):
-        self.distribution_day = stats.norm(33, 6.5)
-        self.distribution_min = stats.norm(863, 190)
+    def __init__(self, ticket_model:str='dev'):
+        self.winnings = 300000
+        self.cost = 3
+        self.moments_day = (33, 6.5)
+        self.moments_min = (863, 190)
+        self.distribution_day = stats.norm(self.moments_day[0],
+                                           self.moments_day[1])
+        self.distribution_min = stats.norm(self.moments_min[0],
+                                           self.moments_min[1])
+        self.set_ticket_model(model_id='dev')
+        self.get_ticket_model()
         self.pickle_jar = "pickles"
         self.pickles = ['breakup_prob_hist',
                         'expected_tickets',
@@ -64,15 +73,56 @@ class Probability:
         p_min = (self.distribution_min.cdf(min)
                  - self.distribution_min.cdf(min - 1))
         return p_day*p_min
+    
+    def set_ticket_model(self, model_id:str='dev'):
+        if model_id == 'dev':
+            dict = {
+                "references": [self.moments_day[0],
+                             self.moments_min[0]],
+                "scaling": [0.1, 0.01],
+                "limits": [0, 100],
+                "thresholds": [0.02, 0.9],
+                "static_method_name": "ticket_model_0",
+                "static_method": self.ticket_model_0
+                }
+            self.ticket_model_dict = dict
+        else:
+            raise ValueError(f"Model ID {model_id} undefined.")
+        
+    def get_ticket_model(self):
+        model_maker = self.ticket_model_dict["static_method"]
+        ticket_model, _, _ = model_maker(self.ticket_model_dict)
+        self.ticket_model = ticket_model
+
+    def get_expected_tickets(self,
+                             argument_1: list,
+                             argument_2: Union[list, None]=None):
+
+        if argument_2 is None:
+            for minute in argument_1:
+                day, min = Minutes.get_day_min(minute)
+        else:
+            day = argument_1
+            min = argument_2
+
+        return self.ticket_model(day, min)
+        
 
     def plot(self, which_pickle:Union[int,str], increment:str='min'):
 
-        breakup_prob_hist = self.pickle_picker('load',
+        data = self.pickle_picker('load',
                                                which_pickle)
+        
+        titles = ['Historic Probability of Breakup',
+                  'Expected Tickets',
+                  'Purchased Tickets']
+        labels = ['Probability', 'Ticket Count', 'Ticket Count']
+
+        if isinstance(which_pickle, str):
+            which_pickle = self.pickles.index(which_pickle)
 
         fig, ax = plt.subplots()
-        
-        
+                
         if increment == 'min':
             pass
             # no longer calculated here
@@ -83,14 +133,14 @@ class Probability:
         else:
             raise ValueError("increment must be 'min' or 'hour'")
         
-        cax = ax.imshow(breakup_prob_hist.T, aspect='auto', cmap='viridis', origin='lower')
-        fig.colorbar(cax, ax=ax, label='Probability')
+        cax = ax.imshow(data.T, aspect='auto', cmap='viridis', origin='lower')
+        fig.colorbar(cax, ax=ax, label=labels[which_pickle])
         
         ax.set_xlim(0, 60)
         ax.set_ylim(0, 24*60)
         ax.set_xlabel('Day Number (April 1 is Day 0)')
         ax.set_ylabel('Minutes of the Day')
-        ax.set_title('Probability of Each Minute')
+        ax.set_title(titles[which_pickle])
         ax.xaxis.set_ticks_position('bottom')
         ax.grid(color='black', linestyle='-', linewidth=0.5)
         ax.minorticks_on()
@@ -158,3 +208,62 @@ class Probability:
                     _ = pickle.load(f)
                     breakup_prob_hist = pickle.load(f)
                 return breakup_prob_hist
+            
+        if pickle_name == 'expected_tickets':
+            if mode == 'save':
+                pickle_state = self.ticket_model_dict.copy()
+                # function can't be pickled, so remove it
+                pickle_state.pop("static_method")
+                expected_tickets = np.zeros((60, 1440))
+                for i in range(60*1440):
+                    day, min = Minutes.get_day_min(i)
+                    expected_tickets[day-1, min] = self.get_expected_tickets(day, min)
+                print(f"Min and max expected tickets: {expected_tickets.min()}, {expected_tickets.max()}")
+                with open('pickles/expected_tickets.pkl', 'wb') as f:
+                    pickle.dump(pickle_state, f)
+                    pickle.dump(expected_tickets, f)
+            elif mode == 'check':
+                with open('pickles/expected_tickets.pkl', 'rb') as f:
+                    _ = pickle.load(f)
+                print(f"Pickle for {pickle_name} is up to date.")
+            elif mode == 'load':
+                with open('pickles/expected_tickets.pkl', 'rb') as f:
+                    _ = pickle.load(f)
+                    expected_tickets = pickle.load(f)
+                return expected_tickets
+
+
+    @staticmethod
+    def ticket_model_0(d: dict):
+        
+        reference_day = d["references"][0]
+        reference_min = d["references"][1]
+        ticket_limits = d["limits"]
+        thresholds = d["thresholds"]
+
+
+        def scale_day(day, scaling_factor=d["scaling"][0]):
+            if day <= reference_day:
+                return np.exp(-scaling_factor * (reference_day - day))
+            else:
+                return np.exp(-scaling_factor * (day - reference_day))
+            
+        def scale_minute(minute, scaling_factor=d["scaling"][1]):
+            if minute <= reference_min:
+                return np.exp(-scaling_factor * (reference_min - minute))
+            else:
+                return np.exp(-scaling_factor * (minute - reference_min))
+
+        def expected_tickets(day, min):
+            day_scale = scale_day(day)
+            min_scale = scale_minute(min)
+            scale = day_scale * min_scale
+            if scale < thresholds[0]:
+                return ticket_limits[0]
+            elif scale > thresholds[1]:
+                return ticket_limits[1]
+            else:
+                return int(round(scale * (ticket_limits[1] - ticket_limits[0]) + ticket_limits[0]))
+            
+        return expected_tickets, scale_day, scale_minute
+            
